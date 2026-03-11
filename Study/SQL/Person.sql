80c94056058cc8ed910b4bfd3a3a686f9673d0f0
정대석 <jds1205jds@gmail.com>
2026-03-11 10:01:06 +0900

    SQL 기본

--- Study/SQL/Person.sql
+++ Study/SQL/Person.sql
@@ -16,37 +16,32 @@ VALUES (2, '강호동', '2000-08-08' );
 INSERT INTO Person (ID, Name, Birthday)
 VALUES (3, '유재석', '1999-01-01')
 
--- 오르차순 생일컬럼 기준 조회
-SELECT Name, Birthday FROM Person
-ORDER BY Birthday ASC;
-
--- 삭제
-DELETE FROM Person
-WHERE ID = 1;
-
--- 업데이트
-UPDATE Person SET Name = '김혜리'
-WHERE Name = '이혜리'
-
-
--- 검색
+-- 조회
 SELECT * FROM Person;
 
-INSERT INTO Person
-VALUES (2, '강호동', '2000-08-08' );
-
--- 삽입
-INSERT INTO Person (ID, Name, Birthday)
-VALUES (3, '유재석', '1999-01-01')
-
--- 오르차순 생일컬럼 기준 조회
+-- 오름차순 조회
 SELECT Name, Birthday FROM Person
 ORDER BY Birthday ASC;
+
+-- 역순출력
+SELECT Name FROM Person ORDER BY Name DESC; 
 
 -- 삭제
 DELETE FROM Person
 WHERE ID = 1;
+DELETE FROM Person WHERE Name='신동엽';
+
 
 -- 업데이트
+UPDATE Person SET Name='강감찬' WHERE ID=2;
 UPDATE Person SET Name = '김혜리'
 WHERE Name = '이혜리'
+
+-- 집게함수
+SELECT count(Height), 
+	max(Height), 
+	min(Height), 
+	avg(Height),
+	sum(Height) 
+FROM Person;
+
