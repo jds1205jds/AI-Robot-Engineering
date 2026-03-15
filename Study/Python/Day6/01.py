cfdfa3c1a400fdc548f68133566ba9102d04c661
정대석 <jds1205jds@gmail.com>
2026-03-15 18:23:17 +0900

    math 모듈 - from, as 구문활용

--- Study/Python/Day6/01.py
+++ Study/Python/Day6/01.py
@@ -1,9 +1,23 @@
-import math as abcde
-from math import sin, cos, tan
-# import tensorflow as tf
-# import numpy as np
 
-print(abcde.sin(1))
+# 기본
+import math
+
+math.sin(1)
+math.cos(1)
+math.tan(1)
+math.floor(2.5) #내림 2
+math.ceil(2.5) #올림 3
+
+# from
+from math import sin, cos, tan
 print(sin(1))
 print(cos(1))
 print(tan(1))
+
+# as
+import math as m
+m.sin(1)
+m.cos(1)
+m.tan(1)
+m.floor(2.5) 
+m.ceil(2.5) 
