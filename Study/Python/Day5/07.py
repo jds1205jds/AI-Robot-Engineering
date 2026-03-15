5449f5cfa452d7abaaf9d5785d330177c910f8fd
정대석 <jds1205jds@gmail.com>
2026-03-15 18:06:45 +0900

    예외처리 고급- raise구문

--- Study/Python/Day5/07.py
+++ Study/Python/Day5/07.py
@@ -1,9 +1,16 @@
-def 나누기(a, b):
-    if b == 0:
-        raise ZeroDivisionError("0으로 나눌 수 없습니다.")
-    return a / b
+number = int(input('정수 입력>' ))
+if number > 0:
+    raise NotImplementedError
+else:
+    raise NotImplementedError
 
+
+-추가 버전-
 try:
-    나누기(10, 0)
-except ZeroDivisionError as e:
-    print(f"경고: {e}")
+    number = int(input('정수 입력>' )) #1.정수가 아닌 값 입력시 오류 발생
+    if number > 0:
+        raise NotImplementedError #2.number가 양수면 예외 발생
+    else:
+        raise NotImplementedError #3.number가 0이거나 음수면 예외 발생
+except:
+    print('예외가 발생했습니다.')
