78780d5f880f9bfb0a0c046a5befc02b247e4cce
정대석 <jds1205jds@gmail.com>
2026-03-15 17:19:35 +0900

    예외처리 고급 - Exception

---
+++ Study/Python/Day5/06.py
@@ -0,0 +1,12 @@
+list_number = [ 52, 273, 32, 72, 100 ]
+
+try :
+  number_input = int(input("정수 입력 : "))
+  print("()번째 요소 : {}".format(number_input, list_number[number_input]))
+except Exception as exception :
+  print("type(exception): ", type(exception))
+  print("exception : ", exception)
+
+
+# Exception은 모든 예외가 발생
+  
